955eccce4c3daae9977bd9b7f0e92fef90d72150
lry <595208882@qq.com>
2019-06-15 15:59:08 +0800

    init

--- micro-doc-generate/src/main/java/org/micro/doc/Constants.java
+++ micro-doc-generate/src/main/java/org/micro/doc/Constants.java
@@ -1,7 +1,5 @@
 package org.micro.doc;
 
-import java.io.File;
-
 /**
  * 常数定义
  * 描述信息1
@@ -14,7 +12,7 @@ public final class Constants {
     public static final String DOT = ".";
     public static final String NULL_STR = "";
     public static final String JAVA_SUFFIX = "java";
-    public static final String MICRO_DOC_JSON = File.separator + "micro-doc.json";
-    public static final String TARGET_CLASSES = File.separator + "target" + File.separator + "classes" + MICRO_DOC_JSON;
+    public static final String MICRO_DOC_JSON = "/micro-doc.json";
+    public static final String TARGET_CLASSES = "/target/classes" + MICRO_DOC_JSON;
 
 }
