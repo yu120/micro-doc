94631cc803cdda8dca70c5e1a696b6ee4ef14de3
lry <595208882@qq.com>
2019-06-15 08:51:02 +0800

    init

--- src/main/java/org/micro/doc/Constants.java
+++ src/main/java/org/micro/doc/Constants.java
@@ -11,20 +11,9 @@ public final class Constants {
 
     public static final String DOT = ".";
     public static final String NULL_STR = "";
-    public static final String API_NOTE_TAG = "apiNote";
-    public static final String SRC_MAIN_JAVA = "/src/main/java/";
     public static final String SEPARATOR = "/";
     public static final String JAVA_SUFFIX = "java";
     public static final String INFO_JSON = SEPARATOR + "info.json";
     public static final String TARGET_CLASSES = SEPARATOR + "target" + SEPARATOR + "classes" + INFO_JSON;
-
-    public static final String CONTENT_TYPE = "Content-type";
-    public static final String API_PATH = "/html/api.html";
-    public static final String MICRO_PAGE_API = "/micro";
-    public static final String PAGE_URL = MICRO_PAGE_API + "/page/*";
-    public static final String PAGE_API_URL = MICRO_PAGE_API + "/page/api";
-    public static final String API_URL = MICRO_PAGE_API + "/api/*";
-    public static final String API_DATA_URL = MICRO_PAGE_API + "/api/data";
-
 
 }
