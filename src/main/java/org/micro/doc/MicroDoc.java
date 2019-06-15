d52b5c4c217c1d355be0b23cef392ddd69d467f2
lry <595208882@qq.com>
2019-06-15 11:35:46 +0800

    init

--- src/main/java/org/micro/doc/MicroDoc.java
+++ src/main/java/org/micro/doc/MicroDoc.java
@@ -32,10 +32,10 @@ public class MicroDoc {
     public static void main(String[] args) throws Exception, NullPointerException {
         String realRootPath = MicroDoc.class.getResource("/").getPath();
         realRootPath = realRootPath.substring(0, realRootPath.lastIndexOf("/target/classes"));
-        execute(realRootPath);
+        System.out.println(execute(realRootPath) ? "成功" : "失败");
     }
 
-    private static void execute(String realRootPath) {
+    private static boolean execute(String realRootPath) {
         String realPath = realRootPath + "/src/main/java";
         log.info("The real path is: {}", realPath);
         PATH = realRootPath + Constants.TARGET_CLASSES;
@@ -51,11 +51,11 @@ public class MicroDoc {
         }
 
         if (javaFiles.size() <= 2) {
-            return;
+            return false;
         }
 
         log.info("The java files: ", javaFiles);
-        com.sun.tools.javadoc.Main.execute(javaFiles.toArray(new String[0]));
+        return com.sun.tools.javadoc.Main.execute(javaFiles.toArray(new String[0])) == 0;
     }
 
     public static JSONArray readInfoJSON() {
