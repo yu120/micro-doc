b7128f0d7b2b3a94e5413c267a4e43faa6386667
lry <595208882@qq.com>
2019-06-15 08:52:10 +0800

    init

--- src/main/java/org/micro/doc/Constants.java
+++ src/main/java/org/micro/doc/Constants.java
@@ -1,5 +1,7 @@
 package org.micro.doc;
 
+import java.io.File;
+
 /**
  * 常数定义
  * 描述信息1
@@ -11,9 +13,8 @@ public final class Constants {
 
     public static final String DOT = ".";
     public static final String NULL_STR = "";
-    public static final String SEPARATOR = "/";
     public static final String JAVA_SUFFIX = "java";
-    public static final String INFO_JSON = SEPARATOR + "info.json";
-    public static final String TARGET_CLASSES = SEPARATOR + "target" + SEPARATOR + "classes" + INFO_JSON;
+    public static final String INFO_JSON = File.separator + "info.json";
+    public static final String TARGET_CLASSES = File.separator + "target" + File.separator + "classes" + INFO_JSON;
 
 }
